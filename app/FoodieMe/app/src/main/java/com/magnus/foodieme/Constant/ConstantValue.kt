7743572ae2a9148c9184f7db1e31af1c3128b951
John Melody Me <johnmelodyme@yandex.com>
2021-02-19 15:18:35 +0800

    #1 Updated

---
+++ app/FoodieMe/app/src/main/java/com/magnus/foodieme/Constant/ConstantValue.kt
@@ -0,0 +1,14 @@
+package com.magnus.foodieme.Constant
+
+class ConstantValue
+{
+    companion object
+    {
+        val AppName: String = "Foodie"
+
+        val MathematicalValue : IntArray = intArrayOf(
+            2000,
+            1000
+        )
+    }
+}
