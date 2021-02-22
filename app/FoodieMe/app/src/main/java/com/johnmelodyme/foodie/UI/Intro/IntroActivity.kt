f42e099a8bbff61dcb5c54337fd87339e550fb25
John Melody Me <Johnmelodyme@yandex.com>
2021-02-22 14:35:13 +0800

    #1 Added UI Component for BottomNavigationBar

---
+++ app/FoodieMe/app/src/main/java/com/johnmelodyme/foodie/UI/Intro/IntroActivity.kt
@@ -0,0 +1,11 @@
+package com.johnmelodyme.foodie.UI.Intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
