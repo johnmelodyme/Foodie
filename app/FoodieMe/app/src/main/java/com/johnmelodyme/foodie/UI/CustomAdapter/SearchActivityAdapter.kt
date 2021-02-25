b857c36f9bcd82d3c4c34efd101b6887f56c3c9f
John Melody Me <Johnmelodyme@yandex.com>
2021-02-26 01:09:36 +0800

    Update SearchActivityAdapter.kt

--- app/FoodieMe/app/src/main/java/com/johnmelodyme/foodie/UI/CustomAdapter/SearchActivityAdapter.kt
+++ app/FoodieMe/app/src/main/java/com/johnmelodyme/foodie/UI/CustomAdapter/SearchActivityAdapter.kt
@@ -1,13 +1,38 @@
 package com.johnmelodyme.foodie.UI.CustomAdapter
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.NonNull
+import com.johnmelodyme.foodie.R
 
 class SearchActivityAdapter(
     @NonNull private val context: Activity,
     private val title: Array<String>,
     private val description: Array<String>,
     private val imgID: Array<Int>
-)
+) : ArrayAdapter<String>(context, R.layout.recipe_qr, title)
 {
+    @SuppressLint("ViewHolder", "InflateParams")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View
+    {
+        val inflater: LayoutInflater = context.layoutInflater
+        val rowView: View = inflater.inflate(R.layout.recipe_qr, null, true)
+
+        val imageView: ImageView = rowView.findViewById<ImageView>(R.id.image_recipe)
+        val titleText: TextView = rowView.findViewById<TextView>(R.id.name_of_recipe)
+        val descriptionText: TextView = rowView.findViewById<TextView>(R.id.description_recipe)
+
+        titleText.text = title[position]
+        imageView.setImageResource(imgID[position])
+        descriptionText.text = description[position]
+
+
+        return rowView
+    }
 }
